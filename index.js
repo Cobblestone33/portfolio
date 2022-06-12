d26ab16a0fa2f8cd57c7df036ed841f7ef54b603
Cobblestone33 <87206376+Cobblestone33@users.noreply.github.com>
2022-06-12 23:45:22 +0200

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-const buttonEl = document.querySelector('a');
-const hiddenEl = document.querySelector('.hidden');
-
-buttonEl.addEventListener('click', () => {
-    hiddenEl.classList.toggle('active');
-})
